a1cbc13b451b50bb3e3732936a33060c27c16260
ygl <lanyueos@qq.com>
2019-08-16 19:23:22 +0800

    完成文件加载

--- src/utils/loader-controller.js
+++ src/utils/loader-controller.js
@@ -1,7 +1,38 @@
 'use strict'
+import camelcase from 'camelcase'
+import loader from './loader'
 
-function loaderController (dir) {
-  // do
+/**
+ * 从controller文件夹加载文件（递归加载）
+ * @param {string} dir
+ * @param {object} options
+ * @returns {Array<{file, name, paths, value}>}
+ * **/
+function loaderController (dir, options = {}) {
+  const files = loader(dir, options)
+  const arr = []
+
+  function load (files, paths = []) {
+    for (const i in files) {
+      const item = files[i]
+      const filename = camelcase(item.name)
+      const cpaths = [].concat(paths, [filename])
+
+      if (item.files && item.files.length > 0) {
+        load(item.files, cpaths)
+      } else {
+        arr.push({
+          ...item,
+          paths: cpaths,
+          value: require(item.filename).default
+        })
+      }
+    }
+  }
+
+  load(files)
+
+  return arr
 }
 
 export default loaderController
--- src/utils/loader.js
+++ src/utils/loader.js
@@ -2,6 +2,37 @@
 import path from 'path'
 import fs from 'fs'
 
-export default function loader (dir) {
-  // do
+/**
+ * 从文件夹里加载指定格式文件
+ * @param {string} dir - 文件夹
+ * @param {{ext: string[], ignore: RegExp[]}} options - 指定扩展文件
+ * @reutrns {Array[]}
+ * **/
+export default function loader (dir, { ext = ['.js'], ignore = [] } = {}) {
+  const files = fs.readdirSync(dir)
+
+  return files.map(filename => {
+    const filepath = path.resolve(dir, filename)
+    const stat = fs.statSync(filepath)
+
+    if (stat.isDirectory()) {
+      return {
+        name: filename,
+        files: loader(filepath, ext)
+      }
+    }
+
+    if (ext.length > 0 && ext.indexOf(path.extname(filename)) === -1) {
+      return null
+    }
+
+    if (ignore.length > 0) {
+      if (ignore.some(e => e.test(filepath))) return null
+    }
+
+    return {
+      filename: filepath,
+      name: path.basename(filename).replace(path.extname(filename), '')
+    }
+  }).filter(e => !!e)
 }
