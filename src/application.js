58f7852d3cb2de6a4d3c2853ad3888089a6dc60a
ygl <lanyueos@qq.com>
2019-08-19 17:44:07 +0800

    添加一些常用中间件

--- src/application.js
+++ src/application.js
@@ -1,9 +1,16 @@
 'use strict'
 import Koa from 'koa'
 import Router from 'koa-router'
+import bodyparser from 'koa-bodyparser'
+import favicon from 'koa-favicon'
+import session from 'koa-session'
+import compress from 'koa-compress'
+import mount from 'koa-mount'
+import KoaStatic from 'koa-static'
 import loaderController from './utils/loader-controller'
 import Controller from './controller'
 import { actionWrapper, pathJoin } from './utils'
+import logger from './middleware/request-log'
 
 class Application extends Koa {
   constructor (options) {
@@ -14,14 +21,73 @@ class Application extends Koa {
     this.router = null
     this.controllers = []
 
+    this.keys = this.config.keys || []
+
     this.initialize()
   }
 
   initialize () {
+    this.initMiddlewares()
+
     const perfix = (this.config.router && this.config.router.perfix) || '/'
     this.router = new Router({ prefix: perfix })
     this.loaderController()
     this.registerRouter()
+  }
+
+  initMiddlewares () {
+    // logger
+    this.use(logger(this.config.logger || {}))
+
+    // gzip
+    if (this.config.gzip) {
+      this.use(compress(typeof this.config.gzip === 'object' ? this.config.gzip : {
+        filter: function (contentType) {
+          return /(html|javascript|image|css)/i.test(contentType)
+        },
+        threshold: 1024,
+        flush: require('zlib').Z_SYNC_FLUSH
+      }))
+    }
+
+    // favicon
+    if (this.config.favicon) {
+      this.use(favicon(this.config.favicon))
+    }
+
+    // static
+    if (this.config.public) {
+      const staticConfig = this.config.public
+
+      if (typeof staticConfig !== 'object') throw new Error('config.public must be an object or array')
+
+      if (Array.isArray(staticConfig)) {
+        for (const i in staticConfig) {
+          this.use(mount(staticConfig[i].publicPath, KoaStatic(staticConfig[i].dir, staticConfig[i].config)))
+        }
+      } else {
+        this.use(mount(staticConfig.publicPath, KoaStatic(staticConfig.dir, staticConfig.config)))
+      }
+    }
+
+    // session
+    if (this.config.session) {
+      this.use(session(this.config.session || {}, this))
+    }
+
+    // bodyparser
+    this.use(bodyparser(this.config.bodyparser))
+
+    // 加载用户中间件
+    if (this.config.middleware && Array.isArray(this.config.middleware)) {
+      for (const i in this.config.middleware) {
+        const middleware = this.config.middleware[i](this)
+
+        if (typeof middleware !== 'function') throw new Error('middleware must be a function')
+
+        this.use(middleware)
+      }
+    }
   }
 
   loaderController () {
